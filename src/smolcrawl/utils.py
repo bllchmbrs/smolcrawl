89d52555452ce653a073d2ff8855c9fb7fb6e3c3
bllchmbrs <bill@hyperlint.com>
2025-04-23 13:54:07 -0700

    fix

--- src/smolcrawl/utils.py
+++ src/smolcrawl/utils.py
@@ -5,16 +5,16 @@ import diskcache
 load_dotenv()
 
 
-DEFAULT_STORAGE_PATH = os.path.join(os.getcwd(), "data")
+DEFAULT_STORAGE_PATH = os.path.join(os.getcwd(), "smolcrawl-data")
 DEFAULT_CACHE_PATH = os.path.join(DEFAULT_STORAGE_PATH, "cache")
 
 
 def get_storage_path() -> str:
-    return os.environ.get("STORAGE_PATH", DEFAULT_STORAGE_PATH)
+    return os.environ.get("SMOLCRAWL_STORAGE_PATH", DEFAULT_STORAGE_PATH)
 
 
 def get_cache_path() -> str:
-    return os.environ.get("CACHE_PATH", DEFAULT_CACHE_PATH)
+    return os.environ.get("SMOLCRAWL_CACHE_PATH", DEFAULT_CACHE_PATH)
 
 
 def get_cache(name: str) -> diskcache.Cache:
